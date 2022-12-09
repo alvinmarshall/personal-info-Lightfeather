b51a363b8c619a5d115f3731ff5f67e80a4dd93f
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-12-09 13:31:16 -0500

    bootstrapping application

---
+++ src/main/java/com/lightfeather/personalinfolightfeather/PersonalInfoLightFeatherApplication.java
@@ -0,0 +1,13 @@
+package com.lightfeather.personalinfolightfeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalInfoLightFeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalInfoLightFeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lightfeather/personalinfolightfeather/PersonalInfoLightFeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightfeather.personalinfolightfeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalInfoLightFeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
